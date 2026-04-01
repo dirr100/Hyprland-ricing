5bca55941ba1b3a370fefbf11b61d1005c900e9d
Danielarch <ttuko711@gmail.com>
2026-04-01 16:04:28 -0600

    prueba script aplicaciones basicas, yay y directorios de cofiguracion

--- scripts.sh/setup.sh
+++ scripts.sh/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Setup main programs
 sudo pacman -Syu --noconfirm
-sudo pacman -S firefox hyprland sddm pavucontrol waybar rofi neovim --noconfirm
+sudo pacman -S firefox hyprland sddm pavucontrol waybar rofi neovim zsh kitty --noconfirm
+# Setup yay
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+
+# Set up directories
+mkdir -p $HOME/.config/hypr/ $HOME/.config/waybar $HOME/.config/nvim $HOME/.config/rofi
